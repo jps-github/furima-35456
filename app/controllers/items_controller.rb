3a28e3290b59ad9be8ee920c7a42cf2c43a73640
jps-github <jumpei.suzuki@di-v.co.jp>
2021-05-24 15:06:39 +0900

    商品一覧表示機能・Rubocopの実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
-
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -21,6 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
+                                 :delivery_day_id, :price).merge(user_id: current_user.id)
   end
 end
